9260761f9b81d0a0faf8f7eddba64051509d2acf
Dari766 <ortegaadriana1865@gmail.com>
2025-03-04 16:30:12 -0500

    Update vwNosotros.php

--- views/vwNosotros.php
+++ views/vwNosotros.php
@@ -1,33 +1,35 @@
-<div class="logo">
-  <img src="Logo.png" alt="">
-</div>
 <div class="container">
-  <h1>Sobre Nosotros - SHOOP Para ti</h1>
+    <div class="row bx-suport">
+        <div class="col bx-form-suport">
 
-  <div class="about-content">
-    <h2>Hecho por Estudiantes, Para Impulsar el Comercio</h2>
-    <p>SHOOP es una tienda online creada por estudiantes del SENA con el propósito de impulsar el comercio digital y
-      ofrecer una plataforma accesible tanto para vendedores como para compradores. Nuestro objetivo principal es ayudar
-      a emprendedores y pequeños negocios a ganar visibilidad, simplificando el proceso de venta de sus productos.</p>
+  <h1>Sobre Nosotros</h1> <h2> SHOOP "Productos sin limites, cerca de ti"</h2>
 
-    <h2>Nuestra Misión</h2>
-    <p>Nuestra misión es fomentar el crecimiento del comercio local mediante una plataforma tecnológica eficiente,
-      diseñada para conectar a vendedores con clientes de manera sencilla. Nos esforzamos por proporcionar herramientas
-      innovadoras que permitan a los emprendedores dar a conocer sus productos, facilitando las ventas y mejorando su
-      alcance.</p>
+            <div class="about-content">
+            <h2>Hecho por Estudiantes, Para Impulsar el Comercio</h2>
+            <p>SHOOP es una tienda online creada por estudiantes del SENA con el propósito de impulsar el comercio digital y
+            ofrecer una plataforma accesible tanto para vendedores como para compradores. Nuestro objetivo principal es ayudar
+            a emprendedores y pequeños negocios a ganar visibilidad, simplificando el proceso de venta de sus productos.</p>
 
-    <h2>Nuestros Productos</h2>
-    <p>En SHOOP, ofrecemos una amplia variedad de artículos seleccionados por nuestros vendedores. Desde dispositivos
-      electrónicos y accesorios para autos y motos, hasta productos únicos y artesanales, nuestra plataforma está
-      diseñada para destacar productos que podrían pasar desapercibidos en otros lugares. Trabajamos con pasión para
-      garantizar que los compradores encuentren artículos de calidad a precios competitivos, mientras ayudamos a los
-      vendedores a crecer y prosperar en un mercado digital.</p>
+            <h2>Nuestra Misión</h2>
+            <p>Nuestra misión es fomentar el crecimiento del comercio local mediante una plataforma tecnológica eficiente,
+            diseñada para conectar a vendedores con clientes de manera sencilla. Nos esforzamos por proporcionar herramientas
+            innovadoras que permitan a los emprendedores dar a conocer sus productos, facilitando las ventas y mejorando su
+            alcance.</p>
 
-    <h2>Contáctenos</h2>
-    <p>Si tiene alguna pregunta sobre nuestros productos o servicios, no dude en ponerse en contacto con nosotros.
-      Estamos aquí para ayudarlo.</p>
-    <p>Dirección: Chía, Cundinamarca</p>
-    <p>Teléfono: +123456789</p>
-    <p>Correo Electrónico: toshoop2024@gmail.com</p>
-  </div>
+            <h2>Nuestros Productos</h2>
+            <p>En SHOOP, ofrecemos una amplia variedad de artículos seleccionados por nuestros vendedores. Desde dispositivos
+            electrónicos y accesorios para autos y motos, hasta productos únicos y artesanales, nuestra plataforma está
+            diseñada para destacar productos que podrían pasar desapercibidos en otros lugares. Trabajamos con pasión para
+            garantizar que los compradores encuentren artículos de calidad a precios competitivos, mientras ayudamos a los
+            vendedores a crecer y prosperar en un mercado digital.</p>
+
+            <h2>Contáctenos</h2>
+            <p>Si tiene alguna pregunta sobre nuestros productos o servicios, no dude en ponerse en contacto con nosotros.
+                Estamos aquí para ayudarlo.</p>
+            <p>Dirección: Chía, Cundinamarca</p>
+            <p>Teléfono: +123456789</p>
+            <p>Correo Electrónico: toshoop2024@gmail.com</p>
+        </div>
+        </div>
+    </div>
 </div>
