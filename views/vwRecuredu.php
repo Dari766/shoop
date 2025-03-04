c5b4401da78768c8a6832a70620462103d85104f
Dari766 <ortegaadriana1865@gmail.com>
2025-03-04 15:40:03 -0500

    Update vwRecuredu.php

--- views/vwRecuredu.php
+++ views/vwRecuredu.php
@@ -1,6 +1,17 @@
-<div class="main-container">
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Recursos educativos</title>
+</head>
+<body>
+  <br>
+  <br>
+  <h1>¿Cómo usar nuestra plataforma?</h1>
+  <div class="main-container">
   <div class="video-container">
-    <iframe width="100%" height="500" src="https://www.youtube.com/embed/dQw4w9WgXcQ" title="YouTube video player"
+    <iframe width="100%" height="500" src="https://www.youtube.com/embed/Nk9oD-iZvhM" title="YouTube video player"
       frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowfullscreen></iframe>
   </div>
@@ -9,4 +20,6 @@
   integrity="sha384-DfXdz2htPH0lsSSGFpoO9xmv/+/z7nU7ELJ6EeAZWlCmGKZk4M1RtIDZOt6Xq/YD" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
   integrity="sha384-eMNCOe7tC1doHpGoJtKh7z7lGz7fuP4F8nfdFvAOA6Gg/z6Y5J6XqqyGXYM2ntX5" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrap
+<script src="https://stackpath.bootstrap"></script>
+</body>
+</html>
